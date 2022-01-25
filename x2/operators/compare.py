fd326cae0aa71b18d159779a2baa6d3b9c2f2b06
iiPython <ben@iipython.cf>
2022-01-24 20:18:52 -0600

    Merge x2.2 to master branch

---
+++ x2/operators/compare.py
@@ -0,0 +1,124 @@
+# Copyright 2022 iiPython
+
+# Modules
+from typing import Any
+
+# Exceptions
+class MissingArguments(Exception):
+    pass
+
+class InvalidArgument(Exception):
+    pass
+
+# Comparator
+_expr_map = {
+    "==": lambda a, b: a == b,
+    "!=": lambda a, b: a != b,
+    ">=": lambda a, b: a >= b,
+    "<=": lambda a, b: a <= b,
+    ">": lambda a, b: a > b,
+    "<": lambda a, b: a < b,
+    "in": lambda a, b: a in b,
+    "xin": lambda a, b: a not in b,
+    "is": lambda a, b: isinstance(a, {"str": str, "float": float, "int": int, "null": None}[b])
+}
+
+# x2 Operators
+class XTOperators:
+    def cmp(ctx) -> None:
+        """
+        Evaluates an expression and executes a branch
+
+        cmp <a> <expr> <b> <branch> [else]
+        a - any
+        expr - expression
+        b - any
+        branch - str
+        else - str
+        """
+        if len(ctx.args) < 4:
+            raise MissingArguments("required: a, expr, b, + branch")
+
+        a, b, expr, branch1, branch2 = ctx.args[0].value, ctx.args[2].value, ctx.args[1].raw, ctx.args[3].value, (ctx.args[4].value if len(ctx.args) > 4 else None)
+        if expr not in _expr_map:
+            raise InvalidArgument("invalid expression")
+
+        if _expr_map[expr](a, b):
+            ctx.memory.interpreter.execute(branch1)
+
+        elif branch2 is not None:
+            ctx.memory.interpreter.execute(branch2)
+
+    def whl(ctx) -> None:
+        """
+        Executes a branch while the provided expression is true
+
+        whl <a> <expr> <b> <branch>
+        a - any
+        expr - expression
+        b - any
+        branch - str
+        """
+        if len(ctx.args) < 4:
+            raise MissingArguments("required: a, expr, b, + branch")
+
+        a, b, expr, branch = ctx.args[0], ctx.args[2], ctx.args[1].raw, ctx.args[3].value
+        if expr not in _expr_map:
+            raise InvalidArgument("invalid expression")
+
+        while _expr_map[expr](a.value, b.value):
+            ctx.memory.interpreter.execute(branch)
+            [_.refresh() for _ in [a, b]]
+
+    def inm(ctx) -> int:
+        """
+        Evaluates whether an object is an integer
+
+        inm <obj> [out]
+        obj - any
+        out - variable
+        """
+        if not ctx.args:
+            raise MissingArguments("required: object")
+
+        val = int(isinstance(ctx.args[0].value, int))
+        if len(ctx.args) > 1:
+            ctx.args[1].set(val)
+
+        return val
+
+    def inms(ctx) -> int:
+        """
+        Evaluates whether an object is an integer (this includes stringified integers)
+        For example, <inms "5"> will return 1 as "5" is counted as an int
+
+        inms <obj> [out]
+        obj - any
+        out - variable
+        """
+        if not ctx.args:
+            raise MissingArguments("required: object")
+
+        if isinstance(ctx.args[0].value, str):
+            try:
+                int(ctx.args[0].value)
+                val = 1
+
+            except ValueError:
+                val = 0
+
+        else:
+            val = int(isinstance(ctx.args[0].value, int))
+
+        if len(ctx.args) > 1:
+            ctx.args[1].set(val)
+
+        return val
+
+# Operator map
+opmap = {
+    "cmp": XTOperators.cmp,
+    "whl": XTOperators.whl,
+    "inm": XTOperators.inm,
+    "inms": XTOperators.inms
+}
---
+++ x2/operators/strcmp.py
@@ -0,0 +1,208 @@
+# Copyright 2022 iiPython
+
+# Exceptions
+class MissingArguments(Exception):
+    pass
+
+class InvalidArgument(Exception):
+    pass
+
+# x2 Operators
+class XTOperators:
+    def lwr(ctx) -> str:
+        """
+        Fully lowercases a given string
+
+        lwr <val> [out]
+        val - str
+        out - variable
+        """
+        if not ctx.args:
+            raise MissingArguments("required: val")
+
+        elif not isinstance(ctx.args[0].value, str):
+            raise InvalidArgument("val must be a string")
+
+        val = ctx.args[0].value.lower()
+        ctx.args[1 if len(ctx.args) > 1 else 0].set(val)
+        return val
+
+    def upr(ctx) -> str:
+        """
+        Fully uppercases a given string
+
+        upr <val> [out]
+        val - str
+        out - variable
+        """
+        if not ctx.args:
+            raise MissingArguments("required: val")
+
+        elif not isinstance(ctx.args[0].value, str):
+            raise InvalidArgument("val must be a string")
+
+        val = ctx.args[0].value.upper()
+        ctx.args[1 if len(ctx.args) > 1 else 0].set(val)
+        return val
+
+    def num(ctx) -> int:
+        """
+        Converts a string to an integer
+
+        num <val> [out]
+        val - str
+        out - variable
+        """
+        if not ctx.args:
+            raise MissingArguments("required: val")
+
+        elif not isinstance(ctx.args[0].value, str):
+            raise InvalidArgument("val must be a string")
+
+        try:
+            val = int(ctx.args[0].value)
+            ctx.args[1 if len(ctx.args) > 1 else 0].set(val)
+            return val
+
+        except ValueError:
+            raise InvalidArgument("val isn't a stringified integer")
+
+    def flt(ctx) -> int:
+        """
+        Converts a string to an float
+
+        num <val> [out]
+        val - str
+        out - variable
+        """
+        if not ctx.args:
+            raise MissingArguments("required: val")
+
+        elif not isinstance(ctx.args[0].value, str):
+            raise InvalidArgument("val must be a string")
+
+        try:
+            val = float(ctx.args[0].value)
+            ctx.args[1 if len(ctx.args) > 1 else 0].set(val)
+            return val
+
+        except ValueError:
+            raise InvalidArgument("val isn't a stringified float")
+
+    def str_(ctx) -> str:
+        """
+        Converts a value to a string
+
+        str <val> [out]
+        val - any
+        out - variable
+        """
+        if not ctx.args:
+            raise MissingArguments("required: val")
+
+        val = str(ctx.args[0].value)
+        ctx.args[1 if len(ctx.args) > 1 else 0].set(val)
+        return val
+
+    def len(ctx) -> int:
+        """
+        Returns the length of a string
+
+        len <val> [out]
+        val - str
+        out - variable
+        """
+        if not ctx.args:
+            raise MissingArguments("required: val")
+
+        elif not isinstance(ctx.args[0].value, str):
+            raise InvalidArgument("val must be a string")
+
+        val = len(ctx.args[0].value)
+        if len(ctx.args) > 1:
+            ctx.args[1].set(val)
+
+        return val
+
+    def slc(ctx) -> str:
+        """
+        Slices a string using the given range
+
+        slc <r1> <r2> <val> [out]
+        r1 - int
+        r2 - int
+        val - str
+        out - variable
+        """
+        if len(ctx.args) < 3:
+            raise MissingArguments("required: r1, r2 + val")
+
+        elif [a for a in ctx.args[:2] if not isinstance(a.value, int)]:
+            raise InvalidArgument("r1 and r2 must both be integers")
+
+        elif not isinstance(ctx.args[2].value, str):
+            raise InvalidArgument("val must be a string")
+
+        val = ctx.args[2].value[ctx.args[0].value:ctx.args[1].value]
+        if len(ctx.args) > 3:
+            ctx.args[3].set(val)
+
+        return val
+
+    def idx(ctx) -> int:
+        """
+        Grabs the index of one string in another string
+
+        idx <str1> <str2> [out]
+        str1 - str
+        str2 - str
+        out - variable
+        """
+        if len(ctx.args) < 2:
+            raise MissingArguments("required: str1, str2")
+
+        elif [a for a in ctx.args[:2] if not isinstance(a.value, str)]:
+            raise InvalidArgument("str1 and str2 must both be strings")
+
+        val = ctx.args[1].value.index(ctx.args[0].value)
+        if len(ctx.args) > 2:
+            ctx.args[2].set(val)
+
+        return val
+
+    def char(ctx) -> str:
+        """
+        Returns the character of a string at the given index
+
+        char <idx> <val> [out]
+        idx - int
+        val - str
+        out - variable
+        """
+        if len(ctx.args) < 2:
+            raise MissingArguments("required: idx + val")
+
+        elif not isinstance(ctx.args[0].value, int):
+            raise InvalidArgument("idx must be an integer")
+
+        elif not isinstance(ctx.args[1].value, str):
+            raise InvalidArgument("val must be a string")
+
+        try:
+            val = ctx.args[1].value[ctx.args[0].value]
+            if len(ctx.args) > 1:
+                ctx.args[1].set(val)
+
+            return val
+
+        except IndexError:
+            raise InvalidArgument("invalid string index")
+
+# Operator map
+opmap = {
+    "lwr": XTOperators.lwr, "upr": XTOperators.upr,
+    "num": XTOperators.num, "flt": XTOperators.flt,
+    "slc": XTOperators.slc, "idx": XTOperators.idx,
+    "len": XTOperators.len, "char": XTOperators.char,
+    "str": XTOperators.str_
+}
