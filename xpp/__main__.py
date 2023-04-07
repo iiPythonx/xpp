404fa8e6aaa3240a3499d5ae07686c19f5038e9d
iiPython <ben@iipython.cf>
2023-04-07 15:08:01 -0500

    [cli] Add xpp -s <module>

--- xpp/__main__.py
+++ xpp/__main__.py
@@ -3,6 +3,7 @@
 # Modules
 import os
 import sys
+import json
 from . import (
     __version__,
     load_sections, config, Interpreter
@@ -19,8 +20,10 @@ class CLI(object):
             {"args": ["-h", "--help"], "fn": self.show_help, "desc": "Displays the help menu"},
             {"args": ["-hl", "--helplong"], "fn": self.show_help_long, "desc": "Displays a more detailed help menu"},
             {"args": ["-v", "--ver", "--version"], "fn": self.show_version, "desc": "Prints the x++ version"},
-            {"args": ["-i", "--installation"], "fn": self.show_install_path, "desc": "Prints the installation path"}
+            {"args": ["-i", "--installation"], "fn": self.show_install_path, "desc": "Prints the installation path"},
+            {"args": ["-s", "--show"], "fn": self.show_module, "desc": "Provides information about an installed x++ module"}
         ]
+        self.install_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 
         self.usage = f"""x++ (x{__version__}) Interpreter
 (c) 2021-23 iiPython; (c) 2022-23 Dm123321_31mD "DmmD" Gaming
@@ -35,15 +38,15 @@ See '{sys.executable} -m xpp -hl' for more detailed usage."""
         for flag in self.flags:
             self.vals[flag["name"]] = any([a in self.argv for a in flag["args"]])
 
-        # Handle options
-        for opt in self.options:
-            if any([a in self.argv for a in opt["args"]]):
-                opt["fn"]()
-
         # Load filepath
         self.filepath = None
         if self.argv:
             self.filepath = ([a for a in self.argv if a[0] != "-"] or [None])[-1]
+
+        # Handle options
+        for opt in self.options:
+            if any([a in self.argv for a in opt["args"]]):
+                opt["fn"]()
 
     def show_help(self) -> None:
         return sys.exit(self.usage)
@@ -64,7 +67,30 @@ See '{sys.executable} -m xpp -hl' for more detailed usage."""
         return sys.exit(__version__)
 
     def show_install_path(self) -> None:
-        return sys.exit(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+        return sys.exit(self.install_path)
+
+    def show_module(self) -> None:
+        module_path = os.path.join(self.install_path, "pkgs", self.filepath.replace(".", os.sep))
+        if not os.path.isdir(module_path):
+            return exit("no such module: " + self.filepath)
+
+        # Load .xconfig
+        xconfig = os.path.join(module_path, ".xconfig")
+        if not os.path.isfile(xconfig) and "." in self.filepath:
+            xconfig = os.path.join(self.install_path, "pkgs", self.filepath.split(".")[-1], ".xconfig")
+
+        metadata = {}
+        if os.path.isfile(xconfig):
+            with open(xconfig, "r") as fh:
+                metadata = json.loads(fh.read())
+
+        # Print out information
+        exit(f"""Name: {self.filepath}
+Version: {metadata.get('version', 'N/A')}
+Summary: {metadata.get('summary', 'N/A')}
+Author: {metadata.get('author', 'N/A')}
+License: {metadata.get('license', 'N/A')}
+Location: {module_path}""")
 
 # Initialization
 cli = CLI()
